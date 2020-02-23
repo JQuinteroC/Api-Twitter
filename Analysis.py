5bfa44afd656a6c7cb0c9ec456df24a0a70a9971
YeikerX117 <52164044+YeikerX117@users.noreply.github.com>
2020-02-23 00:29:52 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -36,13 +36,20 @@ cantidad = int(input("Ingrese la cantidad de Tweets a analizar: "))
 caracter1 = input("Ingrese una parabra clave: ")
 caracter2 = input("Ingrese una parabra clave: ")
 caracter3 = input("Ingrese una parabra clave: ")
+#api.get_user("RamoColombia")
 ###########################Buscar Tweets#######################################
 filtro = busqueda + " -filter:retweets"
 
+
+#caracter(caracter1, caracter2, caracter3, busqueda, cantidad)
+
 for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia",q = filtro, 
                            tweet_mode = "extended").items(cantidad):
+    #print(json.dumps(tweet._json, indent=2))
     api.get_user("RamoColombia").statuses_count
-
+    #print(tweet._json["full_text"])
+    #print()
+#print("\n"*10)
 for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia",
                            q = filtro).items(cantidad):
     #analisis de los tweets 
@@ -77,37 +84,95 @@ plt.axis("equal")
 plt.tight_layout()
 plt.show()
 
-###############################################################################
-##VA DENTRO DE UNA FUNCION 
-filtro2 = busqueda +" " +caracter1
-lista=[]
-suma=0
-for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia", q = filtro2, 
-                          tweet_mode = "extended").items(cantidad):
-    suma += 1
-lista.append(suma)
-    #ista.add.reduce(suma)
+###################Diagrama de barras#########################################
 
-filtro2 = busqueda +" " +caracter2
-suma=0
-for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia", q = filtro2, 
-                           tweet_mode = "extended").items(cantidad):
-    suma += 1
-lista.append(suma)
+users_locs = [[tweet.user.location] for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia", q = "chocoramo", 
+                           tweet_mode = "extended").items(100)]
+#users_locs
 
-    
-filtro2 = busqueda + " " + caracter3
-suma=0
-for tweet in tweepy.Cursor(api.search, screen_name = "RamoColombia", q = filtro2, 
-                           tweet_mode = "extended").items(cantidad):
-    suma += 1
-lista.append(suma)
-    
-ypos = np.arange(cantidad)
-palabraAdicional = [1, 2, 3]
-palabra = [caracter1, caracter2, caracter3]
-prueba = lista
-    
-plt.bar(palabraAdicional, prueba)
-plt.xticks(palabraAdicional, palabra)
+#Analisis a Bogota
+count = 0
+listaC = []
+count += users_locs.count(['Bogotá'])
+count += users_locs.count(['Bogotá, D.C., Colombia'])
+count += users_locs.count(['Bogotá '])
+count += users_locs.count(['Bogotá, D.C.'])
+count += users_locs.count(['Bta'])
+count += users_locs.count(['Bogotá, Colombia'])
+count += users_locs.count(['Bogotá - Colombia'])
+count += users_locs.count(['bogota COLOMBIA'])
+count += users_locs.count(['Madrid / Bogotá D.C., Colombia'])
+listaC.append(count+1)
+
+#Analisis Medellin
+count = 0
+count += users_locs.count(['Medellín, Colombia'])
+count += users_locs.count(['Medellín'])
+count += users_locs.count(['Colombia, Medellin'])
+count += users_locs.count(['Manizales - CO'])
+listaC.append(count+1)
+
+
+#Analisis Barranquilla
+count = 0
+count += users_locs.count(['Barranquilla, Colombia'])
+count += users_locs.count(['Barranquilla'])
+count += users_locs.count(['Colombia, Barranquilla'])
+count += users_locs.count(['Barranquilla - Colombia'])
+count += users_locs.count(['barranquilla - CO'])
+count += users_locs.count(['barranquilla'])
+count += users_locs.count(['Barranquilla '])
+listaC.append(count+1)
+
+#Analisis cali
+count = 0
+count += users_locs.count(['Cali, Colombia'])
+count += users_locs.count(['Cali'])
+count += users_locs.count(['Colombia, Cali'])
+count += users_locs.count(['cali - CO'])
+count += users_locs.count(['Cali - Colombia'])
+count += users_locs.count(['cali'])
+count += users_locs.count(['Cali '])
+listaC.append(count+1)
+
+#Analisis Cartagena
+count = 0
+count += users_locs.count(['Cartagena, Colombia'])
+count += users_locs.count(['Cartagena'])
+count += users_locs.count(['Colombia, Cartagena'])
+count += users_locs.count(['cartagena - CO'])
+count += users_locs.count(['Cartagena - Colombia'])
+count += users_locs.count(['cartagena'])
+count += users_locs.count(['Cartagena '])
+listaC.append(count+1)
+
+##Analisis Cucuta
+count = 0
+count += users_locs.count(['Cucuta, Colombia'])
+count += users_locs.count(['Cucuta'])
+count += users_locs.count(['Colombia, Cucuta'])
+count += users_locs.count(['cucuta - CO'])
+count += users_locs.count(['Cucuta - Colombia'])
+count += users_locs.count(['cucuta'])
+count += users_locs.count(['Cucuta '])
+listaC.append(count+1)
+
+
+valor = 100-sum(listaC)
+listaC.append(valor)
+
+ypos = np.arange(len(listaC))
+palabraAdicional = [0, 1, 2, 3, 4, 5, 6]
+palabra = ["Bogota", "Medellin", "Barranquilla", "Cali", "Cartagena", "Cucuta", "Otro"]
+prueba = listaC
+#Impresion 
+plt.bar(ypos, prueba)
+plt.xticks(ypos, palabra)
 plt.show()
+
+#Filtro para fechas
+for tweet in tweepy.Cursor(api.user_timeline, screen_name="RamoColombia", tweet_mode = "extended",
+                           since="2020-02-20", until="2020-02-22").items(10):
+    print(tweet._json["full_text"])
+    print()
+    
