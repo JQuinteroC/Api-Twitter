85a85e2bffc6e3208c88fc5c251a29e199bdca4f
José Luis Quintero C <jquinteroc@acm.org>
2020-02-24 11:38:55 -0500

    Update form.py

--- form.py
+++ form.py
@@ -6,10 +6,8 @@ from wtforms.fields.html5 import DateField
 
 class CommentForm(Form):
     producto = SelectField('Producto', choices=[
-        ('chocoramo','chocoramo'),('gansito','gansito'),
-        ('ponqué gala','ponqué gala'), ('tostacos','tostacos')])
+        ('chocoramo','chocoramo'),('ponqué gala','ponqué gala'), ('tostacos','tostacos'),
+        ('papas caseras','papas caseras'), ('galletas ramo','galletas ramo')])
     fecha_In = DateField("Fecha ininio", format = '%Y-%m-%d', validators = [validators.Required()])
     fecha_Out = DateField("Fecha fin", format = '%Y-%m-%d', validators = [validators.Required()])
     cantidadTw = IntegerField("Cant. de tweets", [validators.Required(), validators.NumberRange(10,500)])
-
-    
